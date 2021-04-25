79389932a1f11dc5dd38197b4df3d00e282f7282
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-25 14:42:07 -0300

    Creating Controllers

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,9 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from './repositories/SettingsRepository';
+import { SettingsController } from './controllers/SettingsController';
 
 export const router = Router();
 
-router.post('/settings', async (request, response) => {
-  const { chat, username } = request.body;
+const settingsController = new SettingsController();
 
-  const settingsRepository = getCustomRepository(SettingsRepository);
-
-  const settings = settingsRepository.create({
-    chat,
-    username,
-  });
-
-  await settingsRepository.save(settings);
-
-  return response.json(settings);
-
-})
+router.post('/settings', settingsController.create)
 
