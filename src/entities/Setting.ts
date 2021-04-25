7901da9846f253d82420042f1d4f356c52b00aeb
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-25 13:05:56 -0300

    Created entities

---
+++ src/entities/Setting.ts
@@ -0,0 +1,33 @@
+import { 
+  Column, 
+  CreateDateColumn, 
+  Entity, 
+  PrimaryColumn, 
+  UpdateDateColumn 
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('settings')
+export class Setting {
+
+  @PrimaryColumn('uuid')
+  id: string;
+  
+  @Column()
+  username: string;
+
+  @Column()
+  chat: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
+}
