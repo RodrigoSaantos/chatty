0077a821c5a5ad73751c43cd61ce158f16aac51d
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-26 23:07:43 -0300

    Creating connection with socket.io

---
+++ src/http.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import express, { json } from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+import path from 'path';
+
+import './database';
+import { router } from './routes';
+
+const app = express();
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set("views", path.join(__dirname, '..', 'public'));
+app.engine("html", require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/pages/client', (request, response) => {
+  return response.render('html/client.html')
+});
+
+export const http = createServer(app); // Criando protocolo http
+export const io = new Server(http); // Criando protocolo websocket
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
+
+app.use(json());
+app.use(router)
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,5 @@
-import "reflect-metadata";
-import express, { json } from 'express';
-import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
-import path from 'path';
-
-import './database';
-import { router } from './routes';
-
-const app = express();
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set("views", path.join(__dirname, '..', 'public'));
-app.engine("html", require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('/pages/client', (request, response) => {
-  return response.render('html/client.html')
-});
-
-const http = createServer(app); // Criando protocolo http
-const io = new Server(http); // Criando protocolo websocket
-
-io.on("connection", (socket: Socket) => {
-  console.log("Se conectou", socket.id);
-});
-
-app.use(json());
-app.use(router)
+import { http } from "./http";
+import './websockets/client';
 
 http.listen(3333, () => {
   console.log('🚀 App listening on port 3333!');
